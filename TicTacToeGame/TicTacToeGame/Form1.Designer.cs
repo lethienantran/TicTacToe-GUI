d42b3f1a87c2642ab8747702ebc18a3548106248
Le Thien An Tran <114910291+lethienantran@users.noreply.github.com>
2022-10-04 13:39:08 -0700

    Add files via upload

---
+++ TicTacToeGame/TicTacToeGame/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace TicTacToeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grid1 = new System.Windows.Forms.Button();
+            this.grid2 = new System.Windows.Forms.Button();
+            this.grid3 = new System.Windows.Forms.Button();
+            this.grid4 = new System.Windows.Forms.Button();
+            this.grid5 = new System.Windows.Forms.Button();
+            this.grid6 = new System.Windows.Forms.Button();
+            this.grid7 = new System.Windows.Forms.Button();
+            this.grid8 = new System.Windows.Forms.Button();
+            this.grid9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // grid1
+            // 
+            this.grid1.BackColor = System.Drawing.Color.White;
+            this.grid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grid1.Location = new System.Drawing.Point(12, 12);
+            this.grid1.Name = "grid1";
+            this.grid1.Size = new System.Drawing.Size(100, 100);
+            this.grid1.TabIndex = 0;
+            this.grid1.UseVisualStyleBackColor = false;
+            this.grid1.Click += new System.EventHandler(this.grid1_Click);
+            // 
+            // grid2
+            // 
+            this.grid2.BackColor = System.Drawing.Color.White;
+            this.grid2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grid2.Location = new System.Drawing.Point(169, 12);
+            this.grid2.Name = "grid2";
+            this.grid2.Size = new System.Drawing.Size(100, 100);
+            this.grid2.TabIndex = 1;
+            this.grid2.UseVisualStyleBackColor = false;
+            this.grid2.Click += new System.EventHandler(this.grid2_Click);
+            // 
+            // grid3
+            // 
+            this.grid3.BackColor = System.Drawing.Color.White;
+            this.grid3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grid3.Location = new System.Drawing.Point(317, 12);
+            this.grid3.Name = "grid3";
+            this.grid3.Size = new System.Drawing.Size(100, 100);
+            this.grid3.TabIndex = 2;
+            this.grid3.UseVisualStyleBackColor = false;
+            this.grid3.Click += new System.EventHandler(this.grid3_Click);
+            // 
+            // grid4
+            // 
+            this.grid4.BackColor = System.Drawing.Color.White;
+            this.grid4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grid4.Location = new System.Drawing.Point(12, 150);
+            this.grid4.Name = "grid4";
+            this.grid4.Size = new System.Drawing.Size(100, 100);
+            this.grid4.TabIndex = 3;
+            this.grid4.UseVisualStyleBackColor = false;
+            this.grid4.Click += new System.EventHandler(this.grid4_Click);
+            // 
+            // grid5
+            // 
+            this.grid5.BackColor = System.Drawing.Color.White;
+            this.grid5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grid5.Location = new System.Drawing.Point(169, 150);
+            this.grid5.Name = "grid5";
+            this.grid5.Size = new System.Drawing.Size(100, 100);
+            this.grid5.TabIndex = 4;
+            this.grid5.UseVisualStyleBackColor = false;
+            this.grid5.Click += new System.EventHandler(this.grid5_Click);
+            // 
+            // grid6
+            // 
+            this.grid6.BackColor = System.Drawing.Color.White;
+            this.grid6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grid6.Location = new System.Drawing.Point(317, 150);
+            this.grid6.Name = "grid6";
+            this.grid6.Size = new System.Drawing.Size(100, 100);
+            this.grid6.TabIndex = 5;
+            this.grid6.UseVisualStyleBackColor = false;
+            this.grid6.Click += new System.EventHandler(this.grid6_Click);
+            // 
+            // grid7
+            // 
+            this.grid7.BackColor = System.Drawing.Color.White;
+            this.grid7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grid7.Location = new System.Drawing.Point(12, 279);
+            this.grid7.Name = "grid7";
+            this.grid7.Size = new System.Drawing.Size(100, 100);
+            this.grid7.TabIndex = 6;
+            this.grid7.UseVisualStyleBackColor = false;
+            this.grid7.Click += new System.EventHandler(this.grid7_Click);
+            // 
+            // grid8
+            // 
+            this.grid8.BackColor = System.Drawing.Color.White;
+            this.grid8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grid8.Location = new System.Drawing.Point(169, 279);
+            this.grid8.Name = "grid8";
+            this.grid8.Size = new System.Drawing.Size(100, 100);
+            this.grid8.TabIndex = 7;
+            this.grid8.UseVisualStyleBackColor = false;
+            this.grid8.Click += new System.EventHandler(this.grid8_Click);
+            // 
+            // grid9
+            // 
+            this.grid9.BackColor = System.Drawing.Color.White;
+            this.grid9.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grid9.Location = new System.Drawing.Point(317, 279);
+            this.grid9.Name = "grid9";
+            this.grid9.Size = new System.Drawing.Size(100, 100);
+            this.grid9.TabIndex = 8;
+            this.grid9.UseVisualStyleBackColor = false;
+            this.grid9.Click += new System.EventHandler(this.grid9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 390);
+            this.Controls.Add(this.grid9);
+            this.Controls.Add(this.grid8);
+            this.Controls.Add(this.grid7);
+            this.Controls.Add(this.grid6);
+            this.Controls.Add(this.grid5);
+            this.Controls.Add(this.grid4);
+            this.Controls.Add(this.grid3);
+            this.Controls.Add(this.grid2);
+            this.Controls.Add(this.grid1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button grid1;
+        private System.Windows.Forms.Button grid2;
+        private System.Windows.Forms.Button grid3;
+        private System.Windows.Forms.Button grid4;
+        private System.Windows.Forms.Button grid5;
+        private System.Windows.Forms.Button grid6;
+        private System.Windows.Forms.Button grid7;
+        private System.Windows.Forms.Button grid8;
+        private System.Windows.Forms.Button grid9;
+    }
+}
+
---
+++ TicTacToeGame/TicTacToeGame/GamePlay.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToeGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String[] board = new string[9];
+        int currentTurn = 0; 
+        
+        //return player's symbol XO on the board after their turn
+        public String playerSymbol(int turn)
+        {
+            if(turn % 2 == 0 )
+            {
+                return "O";
+            }
+            else
+            {
+                return "X";
+            }
+        }
+        public void WinnerColor(int index)
+        {
+            switch(index)
+            {
+                case 0:
+                    grid1.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 1:
+                    grid2.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 2:
+                    grid3.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 3:
+                    grid4.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 4:
+                    grid5.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 5:
+                    grid6.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 6:
+                    grid7.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 7:
+                    grid8.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 8:
+                    grid9.BackColor = System.Drawing.Color.Red;
+                    break;
+
+            }
+        }
+        public void Restart()
+        {
+            board = new string[9];
+            currentTurn = 0;
+            grid1.Text = "";
+            grid2.Text = "";
+            grid3.Text = "";
+            grid4.Text = "";
+            grid5.Text = "";
+            grid6.Text = "";
+            grid7.Text = "";
+            grid8.Text = "";
+            grid9.Text = "";
+            grid1.BackColor = System.Drawing.Color.White;
+            grid2.BackColor = System.Drawing.Color.White;
+            grid3.BackColor = System.Drawing.Color.White;
+            grid4.BackColor = System.Drawing.Color.White;
+            grid5.BackColor = System.Drawing.Color.White;
+            grid6.BackColor = System.Drawing.Color.White;
+            grid7.BackColor = System.Drawing.Color.White;
+            grid8.BackColor = System.Drawing.Color.White;
+            grid9.BackColor = System.Drawing.Color.White;
+        }
+        public void CheckTie()
+        {
+            int count = 0;
+            for(int i = 0; i < board.Length; i++)
+            {
+                if (board[i] != null)
+                    count++;
+                if(count == 9)
+                {
+                    Restart();
+                    MessageBox.Show("Tie!", "That's a Tie", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+        public void CheckWinner()
+        {
+            int totalWinningPossibility = 8;
+            String winnerString = "";
+            int winTile1 = 0, winTile2 = 0, winTile3 = 0;
+
+            for(int i = 0; i < totalWinningPossibility; i++)
+            {
+                switch(i)
+                {
+                    // 2 diagonal winning cases
+                    case 0:
+                        winnerString = board[0] + board[4] + board[8];
+                        winTile1 = 0; 
+                        winTile2 = 4; 
+                        winTile3 = 8;
+                        break;
+                    case 1:
+                        winnerString = board[2] + board[4] + board[6];
+                        winTile1 = 2;
+                        winTile2 = 4;
+                        winTile3 = 6;
+                        break;
+                    // 3 horizontal winning cases
+                    case 2:
+                        winnerString = board[0] + board[1] + board[2];
+                        winTile1 = 0;
+                        winTile2 = 1;
+                        winTile3 = 2;
+                        break;
+                    case 3:
+                        winnerString = board[3] + board[4] + board[5];
+                        winTile1 = 3;
+                        winTile2 = 4;
+                        winTile3 = 5;
+                        break;
+                    case 4:
+                        winnerString = board[6] + board[7] + board[8];
+                        winTile1 = 6;
+                        winTile2 = 7;
+                        winTile3 = 8;
+                        break;
+                   // 3 vertical winning cases
+                    case 5:
+                        winnerString = board[0] + board[3] + board[6];
+                        winTile1 = 0;
+                        winTile2 = 3;
+                        winTile3 = 6;
+                        break;
+                    case 6:
+                        winnerString = board[1] + board[4] + board[7];
+                        winTile1 = 1;
+                        winTile2 = 4;
+                        winTile3 = 7;
+                        break;
+                    case 7:
+                        winnerString = board[2] + board[5] + board[8];
+                        winTile1 = 2;
+                        winTile2 = 5;
+                        winTile3 = 8;
+                        break;
+                }
+                if(winnerString == "XXX") 
+                {
+                    WinnerColor(winTile1);
+                    WinnerColor(winTile2);
+                    WinnerColor(winTile3);
+                    MessageBox.Show("X won!", "WOOHOO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    Restart();
+                }
+                else if(winnerString == "OOO")
+                {
+                    WinnerColor(winTile1);
+                    WinnerColor(winTile2);
+                    WinnerColor(winTile3);
+                    MessageBox.Show("O won!", "WOOHOO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    Restart();
+                }
+                CheckTie();
+            }
+        }
+
+        private void grid1_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            board[0] = playerSymbol(currentTurn);
+            grid1.Text = board[0];
+            CheckWinner(); 
+        }
+
+        private void grid2_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            board[1] = playerSymbol(currentTurn);
+            grid2.Text = board[1];
+            CheckWinner();
+        }
+
+        private void grid3_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            board[2] = playerSymbol(currentTurn);
+            grid3.Text = board[2];
+            CheckWinner();
+        }
+
+        private void grid4_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            board[3] = playerSymbol(currentTurn);
+            grid4.Text = board[3];
+            CheckWinner();
+        }
+
+        private void grid5_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            board[4] = playerSymbol(currentTurn);
+            grid5.Text = board[4];
+            CheckWinner();
+        }
+
+        private void grid6_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            board[5] = playerSymbol(currentTurn);
+            grid6.Text = board[5];
+            CheckWinner();
+        }
+
+        private void grid7_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            board[6] = playerSymbol(currentTurn);
+            grid7.Text = board[6];
+            CheckWinner();
+        }
+
+        private void grid8_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            board[7] = playerSymbol(currentTurn);
+            grid8.Text = board[7];
+            CheckWinner();
+        }
+
+        private void grid9_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            board[8] = playerSymbol(currentTurn);
+            grid9.Text = board[8];
+            CheckWinner();
+        }
+    }
+}
